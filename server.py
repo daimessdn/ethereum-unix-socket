78fd5ab6c023682351a3ff8f323d5dc9ef8e8405
Dimas W <daimessdn@gmail.com>
2021-02-12 01:04:41 +0700

    small changes for indentation chars and adding another description

--- server.py
+++ server.py
@@ -6,37 +6,35 @@ import os
 print(sys.argv[1])
 
 try:
-	# get a pathname variable
-	## from second argument of program
-	pathname = sys.argv[1]
+    # get a pathname variable from second argument of program
+    pathname = sys.argv[1]
 
-	# delete the old socket
-	## before creating a new socket
-	if (os.path.exists(pathname)):
-		os.remove(pathname)
+    # delete the old socket before creating a new socket
+    if (os.path.exists(pathname)):
+        os.remove(pathname)
 
-	# create a new socket for server
-	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    # create a new socket for server
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-	# bind the socket into pathname
-	s.bind(pathname)
-	print("Creating socket with pathname: %s" % pathname)
+    # bind the socket into pathname
+    ## and make socket listening to incoming client
+    s.bind(pathname)
+    print("Creating socket with pathname: %s" % pathname)
+    s.listen(1)
 
-	# listening for incoming client
-	s.listen(1)
+    # waiting for incoming client
+    while True:
+        # accepting incoming client
+        conn, client = s.accept()
 
-	# waiting for incoming client
-	while True:
-		# accepting incoming client
-		conn, client = s.accept()
-
-		data = conn.recv(1024)
-		print("Getting data from client: %s" % data)
-
+        data = conn.recv(1024)
+        print("Getting data from client: %s" % data)
 
 # in case the arguments is not valid
 except IndexError:
-	print("Invalid arguments (need <pathname> value)")
-	print("$ python3 server.py <pathname>")
+    print("Invalid arguments (need <pathname> value)")
+    print("$ python3 server.py <pathname>")
+
+# exiting program by keyboard input
 except KeyboardInterrupt:
-	print("Shutting down server.")
+    print("Shutting down server.")
