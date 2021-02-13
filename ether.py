ec142643565b5bc909f96c4b0ab015339ca64ec8
Dimas W <daimessdn@gmail.com>
2021-02-14 04:22:46 +0700

    successfully testing for multiple transfer in ether.py

--- ether.py
+++ ether.py
@@ -1,4 +1,5 @@
 # import libraries
+import json
 from web3 import Web3
 
 # init'd Web3 instance to infura URL
@@ -11,48 +12,49 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 sender_address = "0xACa20B87dAEFe8a89f80E15493609bE116c8efc5"
 
 ## get sender private key from a hidden file
-f = open('.%s.txt' % sender_address, "r")
-sender_private_key = f.read()
-f.close()
-
-print(sender_private_key)
+with open('.%s.txt' % sender_address, "r") as f:
+    sender_private_key = f.read()
 
 ## init'd receiver address
 receiver_address = "0xd51e18D83C0e4EeB3D62716eA78Dcc0eB4038a7a"
 
-# # testing web3 operations
-# ## get current block number
-# bn = web3.eth.blockNumber
-# print(bn)
+tx_file = open("transaction_input.json", )
+tx_json = json.load(tx_file)
+tx_file.close()
 
-# ## get current balance for specific address
-# bl1 = web3.eth.getBalance(sender_address)
-# bl2 = web3.fromWei(bl1, "ether")
-
-# print(bl1, bl2)
-
+# transcation using Ethereum
 ## get transaction count
-gtc = web3.eth.getTransactionCount(sender_address)
-print(gtc)
+transaction_count = web3.eth.getTransactionCount(sender_address)
 
-# create transaction
-tx = {
-    "nonce": gtc,
-    "to": receiver_address,
-    "value": web3.toWei(0.001, "ether"),
-    "gas": 200000,
-    "gasPrice": web3.toWei(20, "gwei")
-}
+# # get gas price
+# print(web3.eth.gasPrice)
 
-## signed the transaction we've just created
-signed_tx = web3.eth.account.signTransaction(tx, sender_private_key)
+# create transactions
+transactions = []
 
-# print(signed_tx)
+for index, item in enumerate(tx_json):
+    transactions.append({
+        "nonce": transaction_count + index,
+        "to": item["to_address"],
+        "value": int(item["amount"], 0),
+        "gas": 200000,
+        "gasPrice": web3.toWei(20, "gwei")
+    })
 
-# print(signed_tx.rawTransaction.hex())
-# print(signed_tx.hash.hex())
+for index, tx in enumerate(transactions):
+    signed_tx = web3.eth.account.signTransaction(tx, sender_private_key)
+    sent_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print(web3.toHex(sent_hash))
 
-## send signed transaction to be proceed
-sent_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-print(sent_hash)
-print(web3.toHex(sent_hash))
+# print(transactions)
+
+# ## signed the transaction we've just created
+# signed_tx = web3.eth.account.signTransaction(tx, sender_private_key)
+
+# # print(signed_tx.rawTransaction.hex())
+# # print(signed_tx.hash.hex())
+
+# ## send signed transaction to be proceed
+# sent_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+# print(sent_hash)
+# print(web3.toHex(sent_hash))
