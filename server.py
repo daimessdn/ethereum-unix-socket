658e35812a9e691394effd113cba494e44c91ca2
Dimas W <daimessdn@gmail.com>
2021-02-14 06:35:57 +0700

    changed output in server.py

--- server.py
+++ server.py
@@ -75,11 +75,15 @@ try:
 
             if ("" != data.decode("utf-8")):
                 data = data.decode("utf-8")
-                print("Getting data from client:\n%s" % data)
+                print("\nGetting data from client. Signing a transaction...")
 
+                # convert data into dictionary
+                ## then construct a signed transaction
+                ## to be sent to client
                 json_data = json.loads(data)
                 signed_tx_data = make_signed_tx(json_data)
 
+                print("Transcation signed and has been sent into client. Ready to receive a next input...")
                 conn.send(json.dumps(signed_tx_data).encode("utf-8"))
 
             # in case if client is disconneted
